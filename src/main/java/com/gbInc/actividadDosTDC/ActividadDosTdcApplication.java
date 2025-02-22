e97459c423b3981e55c6ffdef2499a5ec45f1675
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-02-22 15:35:02 -0300

    init: Primer commit, configuracion de dependencias

---
+++ src/main/java/com/gbInc/actividadDosTDC/ActividadDosTdcApplication.java
@@ -0,0 +1,13 @@
+package com.gbInc.actividadDosTDC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadDosTdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadDosTdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbInc/actividadDosTDC/ActividadDosTdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbInc.actividadDosTDC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadDosTdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
