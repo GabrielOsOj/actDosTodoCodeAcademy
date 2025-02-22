3f5b96f3ad4476f4b6693e40208d2d3c218efc8f
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-02-22 16:55:54 -0300

    implementacion de metodos crud para dueños y mascotas

--- src/main/java/com/gbInc/actividadDosTDC/service/mascotaService.java
+++ src/main/java/com/gbInc/actividadDosTDC/service/mascotaService.java
@@ -4,34 +4,50 @@ import com.gbInc.actividadDosTDC.model.Mascota;
 import com.gbInc.actividadDosTDC.repository.ImascotaRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
-public class mascotaService implements ImascotaService{
+public class mascotaService implements ImascotaService {
 
 	@Autowired
 	ImascotaRepository mascotaRepo;
-	
+
 	@Override
 	public boolean crearMascota(Mascota mascota) {
-		
+
+		this.mascotaRepo.save(mascota);
 		return true;
 	}
 
 	@Override
 	public List<Mascota> leerMascotas() {
-	
+
+		return this.mascotaRepo.findAll();
+
 	}
 
 	@Override
 	public boolean actualizarMascota(Mascota mascota) {
-	
-		return true;
+
+		if (this.mascotaRepo.existsById(mascota.getIdMascota())) {
+
+			this.mascotaRepo.save(mascota);
+			return true;
+			
+		}
+		return false;
 	}
 
 	@Override
 	public boolean eliminarMascota(Long idMascota) {
+
+		if(this.mascotaRepo.existsById(idMascota)){
+			
+			this.mascotaRepo.deleteById(idMascota);
+			return true;
+		}
 		
-		return true;
+		return false;
 	}
 
 }
